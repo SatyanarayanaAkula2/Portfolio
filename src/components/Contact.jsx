10e2c1189ad04c0ee8f8d9cdf504890e39eba1ca
Competitivecodelearner <akulasatish49@gmail.com>
2026-02-14 20:08:04 +0530

    reupdated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,8 @@ export default function Contact(){
             <p className='text-xl font-bold text-center'>Contact Me</p>
             <div className=' py-16 flex justify-center '>
                 <div className='w-full max-w-lg bg-white shadow-lg rounded-xl p-16'>
-                    <form className='flex flex-col gap-6'>
+                    <form action="https://api.web3forms.com/submit" method="POST" className='flex flex-col gap-6'>
+                      <input type="hidden" name="access_key" value="4f4e536f-27bf-42fe-93af-082968a6e021"></input>
                         <input className='border border-slate-400 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400' type='text' name='name' placeholder='Enter your name'/>
                         <input className='border border-slate-400 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400' type='email' name='email' placeholder='Enter your email'/>
                         <textarea className='border border-slate-400 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400'  name='message' placeholder="enter Your query/message"/>
